9f7839f2907193055a609d9ae311ded5ada0c69c
Martsim <Martsim@users.noreply.github.com>
2015-04-25 12:57:10 +0300

    Update Main.java
    Added Class User (int id, String name,int phonenr, float longitude, float latitude ).
    Added method get_helpreqdata(phonenr) returns User.
    Updated method find_helper( User requester)
    Added method getchosenOnes( User requester), gives ArrayList of users in range.

--- Main.java
+++ Main.java
@@ -15,19 +15,77 @@ public class Main {
 		
 		while(true){
 			//get request
-			for (int pn: requests){
-				find_helper(pn);
-				
-			}
-			//use method
-						
+			for (int pn: requests){//has to be changed to int i ..x...++. way to remove helped ones
+				User requester = get_helpreqdata(pn);
+				find_helper(requester);
+				//remove this request
+			}		
+		}
+	}
+	
+	private static User get_helpreqdata(int pn) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		int req_phone = 0;
+		int id = 0;
+		String name = "";
+		float latitude = 0;
+		float longitude = 0;
+		
+		
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/t", "", ""); //i think this should have the DB address
+
+		Statement st = con.createStatement();
+		String sql = ("SELECT * FROM user WHERE phonenr = " + Integer.toString(pn) + ";");
+		ResultSet rs = st.executeQuery(sql);
+		
+		if(rs.next()) { 
+			req_phone = rs.getInt("phonenumber");
+			id = rs.getInt("id");
+			name = rs.getString("name");
+			latitude = rs.getFloat("Latitude");
+			longitude = rs.getFloat("Longitude");
+			
+			con.close();
+		}
+		
+		return new User(id, name, req_phone, longitude, latitude);//the error case	
+	}
+	
+	private static void find_helper(User requester) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		//gets chosen people
+		ArrayList<User> users = getchosenOnes(requester);
+		
+		for(User user: users){
+			//TODO: send message to this user part
 		}
 
 	}
 	
-	private static void find_helper(int pn) {
-		// TODO Auto-generated method stub
-		
+	private static ArrayList<User> getchosenOnes(User requester) 
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+
+		ArrayList<User> chosenOnes = new ArrayList<User>();
+
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/t", "", ""); //i think this should have the DB address
+
+		Statement st = con.createStatement();
+		String sql = ("SELECT * FROM user;");
+		ResultSet rs = st.executeQuery(sql);
+		while(rs.next()) { 
+			float Latitude = rs.getFloat("Latitude");
+			float Longitude = rs.getFloat("Longitude");
+			if(distFrom(requester.getLatitude(), requester.getLongitude(), Latitude, Longitude)<1000){
+				int phone = rs.getInt("phonenumber");
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				chosenOnes.add(new User(id, name, phone, Longitude, Longitude));
+			}
+		}
+		con.close();
+		return chosenOnes;
 	}
 	
 	
@@ -48,8 +106,33 @@ public class Main {
 
 }
 
+class User{
+	private int id;
+	private String name;
+	private float longitude;
+	private float latitude;
+	private int phonenr;
 
+	User(int id, String name, int phonenr ,float longitude, float latitude){
+		this.id = id;
+		this.name = name;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.phonenr = phonenr;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public String toString(){
+		return "Name: "+ name + ", N: "+ Float.toString(latitude)+" , E: " + Float.toString(longitude);
+	}
 
+}
+
+//You can ignore this last part, it's and example of a request
 class somecode{
 	static void classmethod() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
 		//first thoughts
@@ -64,12 +147,10 @@ class somecode{
 			Statement st2 = con.createStatement();
 			String sql2 = ("SELECT * FROM User WHERE phonenumber = "+Integer.toString(req_phone)+";");
 			ResultSet rs2 = st2.executeQuery(sql);
-			int id = rs.getInt("first_column_name");
-			String reqName = rs.getString("Name");
-			double loc_N = rs.getDouble("N");
-			double loc_E = rs.getDouble("E");
+			int id = rs.getInt("id");
+			double Latitude = rs.getDouble("Latitude");
+			double Longitude = rs.getDouble("Longitude");
 		}
 		con.close();
 	}
 }
-//http://stackoverflow.com/questions/12041354/java-getting-data-from-mysql-database
